926f171fc79fc00128b06a5890dbfc79dd5a9adc
SkvOl <ghj.ghh.13@mail.ru>
2022-07-11 11:20:52 +0300

    v0.01 base

--- FoodWay/FoodWay/urls.py
+++ FoodWay/FoodWay/urls.py
@@ -2,34 +2,13 @@
 Definition of urls for FoodWay.
 """
 
-from datetime import datetime
-from django.urls import path
+
+from django.urls import include, path
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from Home import forms
-import Home.views as home_viev
-import Way.views as way_view
 
 
 urlpatterns = [
-    path('', home_viev.home, name='home'),
-    path('contact/', home_viev.contact, name='contact'),
-    path('about/', home_viev.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='Home/login.html',
-             authentication_form = forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('reg/', home_viev.register, name='reg'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),   
+    path('', include('Home.urls')),
+    path('way/', include('Way.urls')),
     path('admin/', admin.site.urls),
-    path('profile/', home_viev.profile, name = 'profile'),
-    path('test/', way_view.test),
 ]
---
+++ FoodWay/Home/urls.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from datetime import datetime
+from django.urls import path
+from . import views
+from . import forms
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='Home/login.html',
+             authentication_form = forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('reg/', views.register, name='reg'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('profile/', views.profile, name = 'profile'),
+]
