378b34113241092d3cbb196ce43f00ac8f0b65c7
SkvOl <ghj.ghh.13@mail.ru>
2022-07-14 12:54:41 +0300

    Подключили стили

--- FoodWay/FoodWay/urls.py
+++ FoodWay/FoodWay/urls.py
@@ -1,16 +1,15 @@
-"""
-Definition of urls for FoodWay.
-"""
-
-
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
+from django.views.static import serve 
+from . import settings
 
 
 urlpatterns = [
     path('', include('Home.urls')),
     path('way/', include('Way.urls')),
     path('admin/', admin.site.urls),
+    #re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 handler404 = "FoodWay.views.page_not_found"
