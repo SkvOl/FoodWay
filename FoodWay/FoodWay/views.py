339478768058f2bb3ca6985cea55fd050f51244e
Loks509 <kow20002204@mail.ru>
2022-07-14 13:17:40 +0300

    update pnf

--- FoodWay/FoodWay/views.py
+++ FoodWay/FoodWay/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def page_not_found(request, exception):   
-    return render(request, 'page_not_found.html', status=404)
+    return render(request, 'page_not_found.html',{'title':'Страница не найдена'}, status=404)
