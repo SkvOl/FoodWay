0c75340c9f4bb7cf1d74a70a57315ae3754ebce7
Loks509 <78299514+Loks509@users.noreply.github.com>
2022-07-14 12:38:59 +0300

    Update settings.py

--- FoodWay/FoodWay/settings.py
+++ FoodWay/FoodWay/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ff717049-2462-4527-b0dd-b3b0c748d8af'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
