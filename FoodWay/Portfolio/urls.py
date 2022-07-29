994c6847b1eb6bd851a7bde59259d85c18dabd15
Loks509 <kow20002204@mail.ru>
2022-07-29 21:34:27 +0300

    v0.18 - добавлена возможность удаления фото и загрузка drag-n-drop

--- FoodWay/Portfolio/urls.py
+++ FoodWay/Portfolio/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:id_user>', views.profile, name = 'profile_id'),
     path('edit/', views.edit_profile, name = 'edit_my_profile'),
     path('<int:id_user>/edit/', views.edit_profile, name = 'edit_profile_id'),
-    
+    path('edit_photo/<int:id_user>/', views.edit_photo, name = 'edit_photo'),
 ]
 
--- FoodWay/Portfolio/views.py
+++ FoodWay/Portfolio/views.py
@@ -44,6 +44,7 @@ def handle_uploaded_file(f):
             destination.write(chunk)
 
 def edit_profile(request, id_user = -1):
+    
     if id_user == -1:
         id_user = request.user.id
 
@@ -51,13 +52,13 @@ def edit_profile(request, id_user = -1):
 
         user_edit = User.objects.get(id = id_user)
         user_info = User_info.objects.get_or_create(id_user = user_edit)[0]     #не получилось создать через id_user_(два подчеркивания)_id (что логично в общем-то)
-
+        
         if request.method == 'POST':
             form = User_info_form(request.POST)
-
+            
             if form.is_valid():
                 data = form.cleaned_data
-
+                print(request.FILES)
                 #print(data)
                 #user_info.id_user = user_edit  #возможно надо
                 user_info.id_user.email = data['email']
@@ -69,15 +70,16 @@ def edit_profile(request, id_user = -1):
                 user_info.url_user=data['url_user']
                 user_info.show_email=data['show_email']
                 user_info.about_user=data['about_user']
-                user_info.image_profile = request.FILES['image_profile'];
-                #print(request.FILES)
-                #user_info.id_user.save()
+
+                if request.FILES and request.FILES['image_profile']:
+                    user_info.image_profile = request.FILES['image_profile'];
+                
+                user_info.id_user.save()
                 user_info.save()
 
                 return redirect(f"/profile/{id_user}")
             else:
                 print(form)
-
         else:
             form = User_info_form(user_info.__dict__ | user_edit.__dict__)
 
@@ -88,6 +90,7 @@ def edit_profile(request, id_user = -1):
 
         context = { 
             'form' : form,
+            'id_view_user': user_edit.id,
             'title' : 'Редактирование профиля',
             'year' : datetime.now().year,
             'image_src' : image_src,
@@ -95,3 +98,20 @@ def edit_profile(request, id_user = -1):
         return render(request,'Portfolio/form.html', context)
     else:
         return page_not_found(request, None)
+
+def edit_photo(request, id_user = -1):
+    if request.method == 'POST':    #по какой-то причина в jquery/ajax появляется ошибка
+        if id_user == -1:
+            id_user = request.user.id
+
+        if User.objects.filter(id = id_user).exists():
+            user_info = User_info.objects.get(id_user__id = id_user)
+
+            if request.POST.get('delete')=='true':
+                user_info.image_profile = None
+                user_info.save()
+            elif request.POST.get('delete')=='false' and request.FILES and request.FILES['image_profile']:
+                user_info.image_profile = request.FILES['image_profile']
+                user_info.save()
+
+    return JsonResponse({'stasus':'ok'}, safe=False)
